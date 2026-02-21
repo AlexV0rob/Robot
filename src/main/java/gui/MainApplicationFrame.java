88a9a4d2d31df449aa4b07885086c9b3b15be0fa
aleks <aleks@MSI>
2026-02-21 17:18:13 +0500

    Меню в MainApplicationFrame теперь создаётся несколькими методами
    Метод в MainApplicationFrame, отвечающий за создание меню, разбит на:
    1) метод, создающий само меню (generateMenuBar);
    2) метод, создающий вкладку с изменением внешнего вида
    (generateViewMenu);
    3) метод, создающий вкладку с тестами (generateTestsMenu);
    4) метод, создающий элемент вкладки меню (createMenuItem).

--- src/main/java/gui/MainApplicationFrame.java
+++ src/main/java/gui/MainApplicationFrame.java
@@ -4,6 +4,7 @@ import log.Logger;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 /**
@@ -56,44 +57,69 @@ public class MainApplicationFrame extends JFrame
         frame.setVisible(true);
     }
 
+    /**
+     * Создать элемент меню
+     */
+    private JMenuItem createMenuItem(String name, int mnemonic, 
+    		ActionListener actionListener) {
+    	JMenuItem menuItem = new JMenuItem(name, mnemonic);
+        menuItem.addActionListener(actionListener);
+        return menuItem;
+    }
+    
+    /**
+     * Сгенерировать меню с настройками отображения
+     */
+    private JMenu generateViewMenu() {
+    	JMenu menu = new JMenu("Режим отображения");
+        menu.setMnemonic(KeyEvent.VK_V);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "Управление режимом отображения приложения");
+        
+        menu.add(createMenuItem(
+        		"Системная схема",
+        		KeyEvent.VK_S,
+        		(event) -> {
+        			setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        			this.invalidate();
+        		}));
+
+        menu.add(createMenuItem(
+        		"Универсальная схема",
+        		KeyEvent.VK_S,
+        		(event) -> {
+        			setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        			this.invalidate();
+        		}));
+        return menu;
+    }
+    
+    /**
+     * Сгенерировать меню с тестами
+     */
+    private JMenu generateTestsMenu() {
+    	JMenu menu = new JMenu("Тесты");
+        menu.setMnemonic(KeyEvent.VK_T);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "Тестовые команды");
+        
+        menu.add(createMenuItem(
+        		"Сообщение в лог",
+        		KeyEvent.VK_S,
+        		(event) -> {
+        			Logger.debug("Новая строка");
+        		}));
+        return menu;
+    }
+    
     private JMenuBar generateMenuBar()
     {
         JMenuBar menuBar = new JMenuBar();
-        
-        JMenu lookAndFeelMenu = new JMenu("Режим отображения");
-        lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
-        lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
-                "Управление режимом отображения приложения");
-        
-        JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
-        systemLookAndFeel.addActionListener((event) -> {
-            setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            this.invalidate();
-        });
-        lookAndFeelMenu.add(systemLookAndFeel);
-
-        JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
-        crossplatformLookAndFeel.addActionListener((event) -> {
-            setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-            this.invalidate();
-        });
-        lookAndFeelMenu.add(crossplatformLookAndFeel);
-
-        JMenu testMenu = new JMenu("Тесты");
-        testMenu.setMnemonic(KeyEvent.VK_T);
-        testMenu.getAccessibleContext().setAccessibleDescription(
-                "Тестовые команды");
-        
-        JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
-        addLogMessageItem.addActionListener((event) -> {
-            Logger.debug("Новая строка");
-        });
-        testMenu.add(addLogMessageItem);
-
-        menuBar.add(lookAndFeelMenu);
-        menuBar.add(testMenu);
+        menuBar.add(generateViewMenu());
+        menuBar.add(generateTestsMenu());
         return menuBar;
     }
+
     
     private void setLookAndFeel(String className)
     {
